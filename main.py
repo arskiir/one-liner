504668e2722553b220b671767d1d8f232aab6d0a
Arm Sukrit <arm.xdsk@hotmail.com>
2021-04-03 06:39:22 +0700

    paste code in .txt and that's it

--- main.py
+++ main.py
@@ -3,27 +3,34 @@
 # ! here https://onlineunicodetools.com/convert-unicode-to-string-literal
 
 import sys
+import os
+if os.name == 'nt':
+    import msvcrt
+else:
+    import termios
 
 
 def main():
-    if len(sys.argv) == 2:
-        print_one_liner(sys.argv[1])
-    elif len(sys.argv) == 1:
-        unicode_str = input("Enter unicode escape sequence: ")
-        # example, Enter: \u0068\u0065\u006c\u006c\u006f
-        print_one_liner(unicode_str)
-    else:
-        exit(1)
+    raw_code_file = "code.txt"
+    if not os.path.exists(raw_code_file):
+        open(raw_code_file, "w").close()
+
+    wait_key("Press any key to open file, paste code, save, then return here")
+    os.system("start " + raw_code_file)
+    wait_key("Press any key to continue")
+    
+    processed_code_file = "one-line-code.txt"
+    with open(raw_code_file) as f:
+        processed_string = transform_one_line(f.read())
+        print(processed_string)
+        with open(processed_code_file, 'w') as p:
+            p.write(processed_string)
+    os.system("start " + processed_code_file)
 
 
-def print_one_liner(uni: str) -> None:
-    """process "str" of uni code escape characters 
-    and print them in unicode literal with newline and tab as \n and \t
-    """
+def transform_one_line(string: str) -> str:
+    """ process text with newline and tab characters as \n and \t """
 
-    list_of_ascii_hex = uni.replace("\\u", " ").split()
-    string = "".join(
-        list(map(lambda hex_string: chr(int(hex_string, 16)), list_of_ascii_hex)))
     processed_string = ""
     for each in string:
         if each == '\n':
@@ -33,8 +40,36 @@ def print_one_liner(uni: str) -> None:
             processed_string += "\\t"
             continue
         processed_string += each
-    print(processed_string)
+    return processed_string
+
+
+def wait_key(prompt=None, end='\n'):
+    """ Wait for a key press on the console and return it as str type. """
+
+    if prompt:
+        print(prompt, end=end)
+
+    result = None
+    if os.name == 'nt':
+        result = msvcrt.getch()
+    else:
+        fd = sys.stdin.fileno()
+
+        oldterm = termios.tcgetattr(fd)
+        newattr = termios.tcgetattr(fd)
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+        try:
+            result = sys.stdin.read(1)
+        except IOError:
+            pass
+        finally:
+            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+
+    return result if type(result) == str else result.decode('utf-8')
 
 
 if __name__ == "__main__":
     main()
+
