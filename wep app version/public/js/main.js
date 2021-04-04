5f9dd2fe640031c224c4893fb8af8548090e5700
Arm Sukrit <arm.xdsk@hotmail.com>
2021-04-04 21:54:50 +0700

    can use tab now within textarea and change oninput to onkeyup

--- wep app version/public/js/main.js
+++ wep app version/public/js/main.js
@@ -1,3 +1,19 @@
+const textareas = document.getElementsByTagName("textarea");
+const count = textareas.length;
+for (var i = 0; i < count; i++) {
+    textareas[i].onkeydown = function (e) {
+        if (e.key == 9 || e.which == 9) {
+            e.preventDefault();
+            var s = this.selectionStart;
+            this.value =
+                this.value.substring(0, this.selectionStart) +
+                "\t" +
+                this.value.substring(this.selectionEnd);
+            this.selectionEnd = s + 1;
+        }
+    };
+}
+
 const updateOutput = (text) => {
     const oneLiner = makeOneLine(text);
     document.getElementById("one-liner").value = oneLiner;
